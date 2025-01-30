63d9a19f26b9dc46556925c267a3ef45f9098dd6
yagoalura <yagoliveira92@gmail.com>
2025-01-30 09:23:02 -0300

    local_adapter created

---
+++ lib/src/core/local_storage_adapter/local_storage_adapter.dart
@@ -0,0 +1,12 @@
+abstract class LocalStorageAdapter {
+  Future<void> start();
+
+  Future<Map<String, dynamic>> get({
+    required String store,
+  });
+
+  Future<void> eraseAndAdd({
+    required String store,
+    required Map<String, dynamic> data,
+  });
+}
