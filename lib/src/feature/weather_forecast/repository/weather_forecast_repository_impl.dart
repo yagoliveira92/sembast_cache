07578c5ac351fcf6ce4aab4e30efa80578095c08
Yago Souza Oliveira <yagoliveira92@gmail.com>
2025-02-06 11:05:30 -0300

    Limpando o Sembast do Projeto para poder começar

--- lib/src/feature/weather_forecast/repository/weather_forecast_repository_impl.dart
+++ lib/src/feature/weather_forecast/repository/weather_forecast_repository_impl.dart
@@ -1,19 +1,16 @@
 import 'package:sembast_cache/src/core/models/weather_model.dart';
 import 'package:sembast_cache/src/core/services/connectivity_service.dart';
-import 'package:sembast_cache/src/core/services/local_data_service.dart';
 import 'package:sembast_cache/src/core/services/weather_api_service.dart';
 import 'package:sembast_cache/src/feature/weather_forecast/repository/weather_forecast_repository.dart';
 
 class WeatherRepositoryImpl implements WeatherRepository {
-  final LocalDataService _localDataService;
   final WeatherApiService _weatherApiService;
   final ConnectivityService _connectivityService;
 
   WeatherRepositoryImpl(
-      this._localDataService,
-      this._weatherApiService,
-      this._connectivityService,
-      );
+    this._weatherApiService,
+    this._connectivityService,
+  );
 
   @override
   Future<List<WeatherModel>> getWeatherData() async {
@@ -22,20 +19,15 @@ class WeatherRepositoryImpl implements WeatherRepository {
     List<WeatherModel> weatherData = [];
     if (hasConnection) {
       await Future.forEach(cities, (city) async {
-       try {
-         final data = await _weatherApiService.fetchData(city);
-         weatherData.add(data);
-         await _localDataService.saveData('weather_store_$city', data.toMap());
-       } catch (e) {
-         final localData = await _localDataService.getData('weather_store_$city');
-         weatherData.add(WeatherModel.fromJson(localData),);
-       }
+        try {
+          final data = await _weatherApiService.fetchData(city);
+          weatherData.add(data);
+        } catch (e) {
+          print(e);
+        }
       });
     } else {
-      final localData = await _localDataService.getData('weather_store');
-      if (localData.isEmpty) {
-        throw Exception('No local data available');
-      }
+      throw Exception('No internet connection');
     }
     return weatherData;
   }
